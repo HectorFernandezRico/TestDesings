186768ede0bfe308022cd1a0d41aa7ff94fbba43
HectorFernandezRico <hectorfernandezrico@gmail.com>
2024-02-22 14:06:39 +0100

    Trabajo Finalizado

--- TestDesings/src/test/java/SelectorTests.java
+++ TestDesings/src/test/java/SelectorTests.java
@@ -1,5 +1,5 @@
 
-import com.mycompany.testdesings.GestorPalabras;
+import com.mycompany.testdesings.*;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
@@ -8,23 +8,50 @@ import org.junit.Test;
  */
 
 public class SelectorTests {
-
+    
     @Test //Funciona correctamente.
     public void esPalindromo() {      
         GestorPalabras palabra = new GestorPalabras();
-        assertEquals(true, palabra.esPalindromo("allí ves sevilla"));	     
+        assertEquals(true, palabra.esPalindromo("allí ves sevilla"));
+	assertEquals(false, palabra.esPalindromo("jesús todo poderoso"));	
+	assertEquals(true, palabra.esPalindromo("amo la paloma"));
+	assertEquals(false, palabra.esPalindromo("Dios es misericordioso"));	 
+	assertEquals(true, palabra.esPalindromo("A luna ese anula"));
+	assertEquals(false, palabra.esPalindromo("Todo el mundo merece una segunda oportunidad")); 
+	assertEquals(true, palabra.esPalindromo("A mi loca Colima"));
+	assertEquals(false, palabra.esPalindromo("La fé es lo último que se pierde")); 
+	assertEquals(true, palabra.esPalindromo("A ti no, bonita"));	     
     }
 
-    @Test //Funciona correctamente.
-    public void contarVocales() { 
+    @Test
+    public void contarVocales() { //Funciona correctamente.
         GestorPalabras palabra = new GestorPalabras();
-        assertEquals(true, palabra.esPalindromo("allí ves sevilla"));
+        assertEquals(16, palabra.contarVocales("Toji Fushiguro Mejor qúe tu novió GEGE"));
+        assertEquals(6, palabra.contarVocales("Algó ándaba mal"));
+        assertEquals(14, palabra.contarVocales("Péro mí egó me condeno a la derrota"));
+        assertEquals(9, palabra.contarVocales("Expasión de dominio"));
+        assertEquals(12, palabra.contarVocales("Yutaaaá Ganá a Sùkúná yá"));
+        assertEquals(4, palabra.contarVocales("fucking panza"));
+        assertEquals(3, palabra.contarVocales("añañin"));
+        assertEquals(29, palabra.contarVocales("Pènse que había elegido una vida en la que no me comparía còn lós demás"));
+        assertEquals(12, palabra.contarVocales("HenriGánòLáPèlèáContráDIO"));
+        assertEquals(12, palabra.contarVocales("No copie yá qué lá notá será"));
+        assertEquals(7, palabra.contarVocales("Cuèstiónable nó"));
     }
     
-    @Test //Funciona correctamente.
-    public void invertirPalabra() {
+    @Test
+    public void invertirPalabra() { //Funciona correctamente.
         GestorPalabras palabra = new GestorPalabras();
         assertEquals("amabo", palabra.invertirPalabra("obama"));
+        assertEquals("ordep", palabra.invertirPalabra("Pedro"));
+        assertEquals("saiasi", palabra.invertirPalabra("Isaias"));
+        assertEquals("sesiom", palabra.invertirPalabra("Moises"));
+        assertEquals("obocaj", palabra.invertirPalabra("Jacobo"));
+        assertEquals("amasimak", palabra.invertirPalabra("Kamisama"));
+        assertEquals("leba", palabra.invertirPalabra("Abel"));
+        assertEquals("talcobmob", palabra.invertirPalabra("Bomboclat"));
+        assertEquals("niac", palabra.invertirPalabra("Cain"));
+        assertEquals("saduj", palabra.invertirPalabra("Judas"));
     }
    
 }
